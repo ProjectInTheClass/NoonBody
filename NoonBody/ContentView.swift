5d4f053c3a03218005c6deb7f8080dccfd59835f
Yoon Jinyeong <wwwz4805@naver.com>
2020-09-29 16:10:52 +0900

    Initial commit

---
+++ NoonBody/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NoonBody
+//
+//  Created by 윤진영 on 2020/09/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
