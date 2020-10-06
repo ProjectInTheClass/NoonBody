282c88e0c6da5d98703be3d5bee81af183ad8f24
Yoon Jinyeong <wwwz4805@naver.com>
2020-10-06 15:59:56 +0900

    Create NoonBodyWriteMain.swift

---
+++ NoonBody/NoonBodyWriteMain.swift
@@ -0,0 +1,20 @@
+//
+//  NoonBodyWriteMain.swift
+//  NoonBody
+//
+//  Created by 윤진영 on 2020/10/06.
+//
+
+import SwiftUI
+
+struct NoonBodyWriteMain: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NoonBodyWriteMain_Previews: PreviewProvider {
+    static var previews: some View {
+        NoonBodyWriteMain()
+    }
+}
