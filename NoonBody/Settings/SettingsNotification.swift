d26390d8196a1ab83c4e912d083392e68eb9984f
Jenny <Hyunjennie@gmail.com>
2020-10-11 18:10:34 +0900

    Create subpages for Noonbody Settings

---
+++ NoonBody/Settings/SettingsNotification.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsNotification.swift
+//  NoonBody
+//
+//  Created by Jen on 2020-10-11.
+//
+
+import SwiftUI
+
+struct SettingsNotification: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsNotification_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsNotification()
+    }
+}
