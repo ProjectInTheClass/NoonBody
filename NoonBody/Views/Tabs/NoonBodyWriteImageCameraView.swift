81d46db4e79a98dc181b0e23276fc5d97da8fca0
Yoon Jinyeong <wwwz4805@naver.com>
2020-10-11 20:18:50 +0900

    Update NoonBodyWriteImageCameraView.swift

--- NoonBody/Views/Tabs/NoonBodyWriteImageCameraView.swift
+++ NoonBody/Views/Tabs/NoonBodyWriteImageCameraView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+
 struct NoonBodyWriteImageCameraView: View {
     @State private var showYPImagePickerView = true
 
@@ -17,58 +18,17 @@ struct NoonBodyWriteImageCameraView: View {
                 }, label: { Text("Show Square").font(.title) })
 
                 if showYPImagePickerView {
-                    YPImagePickerView()
+                   
                 }
             }
 
         }
 }
 
-struct YPImagePickerView: UIViewControllerRepresentable
-{
-    func makeUIViewController(context: Context) -> YPImagePickerViewController {
-        let vc =  YPImagePickerViewController()
-        print("\nmakeUIViewController \(vc)")
-        return vc
+
+
+struct NoonBodyWriteImageCameraView_Previews: PreviewProvider {
+    static var previews: some View {
+        NoonBodyWriteImageCameraView()
     }
-
-    func updateUIViewController(_ uiViewController: YPImagePickerViewController, context: Context) {
-        print("updateUIViewController \(uiViewController)")
-    }
-
-    static func dismantleUIViewController(_ uiViewController: YPImagePickerViewController, coordinator: Self.Coordinator) {
-        print("dismantleUIViewController \(uiViewController)")
-    }
-
-}
-
-class YPImagePickerViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = UIColor.green
-        print("viewDidLoad \(self)")
-        let picker = YPImagePicker()
-        picker.didFinishPicking { [unowned picker] items, _ in
-        if let photo = items.singlePhoto {
-            print(photo.fromCamera) // Image source (camera or library)
-            print(photo.image) // Final image selected by the user
-            print(photo.originalImage) // original image selected by the user, unfiltered
-            print(photo.modifiedImage) // Transformed image, can be nil
-            print(photo.exifMeta) // Print exif meta data of original image.
-    }
-    picker.dismiss(animated: true, completion: nil)
-}
-present(picker, animated: true, completion: nil)
-    }
-
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        print("viewWillDissapear \(self)")
-    }
-
-    deinit {
-        print("DEINIT \(self)")
-    }
-
 }
