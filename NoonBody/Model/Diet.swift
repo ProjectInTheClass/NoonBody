3ff64d88c8bed9b422b92415b6dddcbc972ddd46
Yoon Jinyeong <wwwz4805@naver.com>
2020-10-12 11:15:04 +0900

    Create Diet Model

---
+++ NoonBody/Model/Diet.swift
@@ -0,0 +1,8 @@
+//
+//  Diet.swift
+//  NoonBody
+//
+//  Created by 윤진영 on 2020/10/12.
+//
+
+import Foundation
---
+++ NoonBody/Model/DietStore.swift
@@ -0,0 +1,8 @@
+//
+//  DietStore.swift
+//  NoonBody
+//
+//  Created by 윤진영 on 2020/10/12.
+//
+
+import Foundation
