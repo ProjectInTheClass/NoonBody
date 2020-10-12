3939d559a28e8139b7c0688e81777feb15bb1f0e
Yoon Jinyeong <wwwz4805@naver.com>
2020-10-12 13:30:45 +0900

    Create CommunityTab.swift

---
+++ NoonBody/Views/Tabs/Community/CommunityTab.swift
@@ -0,0 +1,20 @@
+//
+//  CommunityTab.swift
+//  NoonBody
+//
+//  Created by 윤진영 on 2020/10/12.
+//
+
+import SwiftUI
+
+struct CommunityTab: View {
+    var body: some View {
+        Text("커뮤니티 탭")
+    }
+}
+
+struct CommunityTab_Previews: PreviewProvider {
+    static var previews: some View {
+        CommunityTab()
+    }
+}
