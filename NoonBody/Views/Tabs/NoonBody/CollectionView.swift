565ae8b8474b029d59dbacdecb64a454505ef790
Yoon Jinyeong <wwwz4805@naver.com>
2020-10-12 17:50:45 +0900

    Update CollectionView.swift

--- NoonBody/Views/Tabs/NoonBody/CollectionView.swift
+++ NoonBody/Views/Tabs/NoonBody/CollectionView.swift
@@ -12,43 +12,45 @@ struct CollectionView: View {
     
     var body: some View {
             
-        VStack (alignment: .leading){
-            HStack{
-                    VStack{
-                        Text("어제")
+        ScrollView {
+            VStack (alignment: .leading){
+                HStack{
+                        VStack{
+                            Text("어제")
+                                
+                            Image("food1")
+                                .resizable()
+                                .frame(width: 200, height: 400, alignment: .trailing)
+                        }
+                        VStack{
+                            Text("오늘")
+                                
+                            Image("food2")
+                                .resizable()
+                                .frame(width: 200, height: 400, alignment: .trailing)
                             
-                        Image("food1")
-                            .resizable()
-                            .frame(width: 200, height: 400, alignment: .trailing)
-                    }
-                    VStack{
-                        Text("오늘")
-                            
-                        Image("food2")
-                            .resizable()
-                            .frame(width: 200, height: 400, alignment: .trailing)
-                        
-                    }
-                      
-            }
+                        }
+                          
+                }
 
-            HStack{
-                Text("과거의 나")
-                    .font(.system(size: 20))
-                    .padding(.trailing, 200)
+                HStack{
+                    Text("과거의 나")
+                        .font(.system(size: 20))
+                        .padding(.trailing, 200)
+                        
                     
+                    DatePicker("", selection: $date, displayedComponents: .date)
+                        .datePickerStyle(DefaultDatePickerStyle())
+                        .labelsHidden()
+                        .frame(maxHeight: .none)
+                    
+                }.padding(.top, 40)
                 
-                DatePicker("", selection: $date, displayedComponents: .date)
-                    .datePickerStyle(DefaultDatePickerStyle())
-                    .labelsHidden()
-                    .frame(maxHeight: .none)
                 
-            }.padding(.top, 40)
-            
-            
-            //갤러리
-            
-            
+                //갤러리
+                
+                
+            }
         }
             
         }
