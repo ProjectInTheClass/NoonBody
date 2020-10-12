0010e2767175d32f505fa57e074c262850392749
Yoon Jinyeong <wwwz4805@naver.com>
2020-10-12 17:42:51 +0900

    Update CollectionView.swift

--- NoonBody/Views/Tabs/NoonBody/CollectionView.swift
+++ NoonBody/Views/Tabs/NoonBody/CollectionView.swift
@@ -8,10 +8,52 @@
 import SwiftUI
 
 struct CollectionView: View {
+    @State var date = Date()
+    
     var body: some View {
-        Text("모아보기")
+            
+        VStack (alignment: .leading){
+            HStack{
+                    VStack{
+                        Text("어제")
+                            
+                        Image("food1")
+                            .resizable()
+                            .frame(width: 200, height: 400, alignment: .trailing)
+                    }
+                    VStack{
+                        Text("오늘")
+                            
+                        Image("food2")
+                            .resizable()
+                            .frame(width: 200, height: 400, alignment: .trailing)
+                        
+                    }
+                      
+            }
+
+            HStack{
+                Text("과거의 나")
+                    .font(.system(size: 20))
+                    .padding(.trailing, 200)
+                    
+                
+                DatePicker("", selection: $date, displayedComponents: .date)
+                    .datePickerStyle(DefaultDatePickerStyle())
+                    .labelsHidden()
+                    .frame(maxHeight: .none)
+                
+            }.padding(.top, 40)
+            
+            
+            //갤러리
+            
+            
+        }
+            
+        }
+        
     }
-}
 
 struct CollectionView_Previews: PreviewProvider {
     static var previews: some View {
