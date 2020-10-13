fefa6f55f2cbff6ac2aa882fa41c32fd158467ab
Yoon Jinyeong <wwwz4805@naver.com>
2020-10-13 20:13:05 +0900

    Delete models

--- NoonBody/Model/NoonBody.swift
+++ NoonBody/Model/NoonBody.swift
@@ -7,25 +7,3 @@
 import Foundation
 
 
-class NoonBody: Identifiable, ObservableObject{
-    
-    let id: UUID
-    
-    @Published var content: String
-    
-    let insertDate: Date
-    
-    
-    init(id: UUID = UUID(), content: String, insertDate: Date = Date()) {
-        self.id = id
-        self.content = content
-        self.insertDate = insertDate
-    }
-}
-
-//새로운 extention을 추가하고 Equatable 프로토콜을 구현
-extension NoonBody: Equatable{
-    static func == (lhs: NoonBody, rhs: NoonBody) -> Bool{
-        return lhs.id == rhs.id  //id 속성값만 비교
-    }
-}
