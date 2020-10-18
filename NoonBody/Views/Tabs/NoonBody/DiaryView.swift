95097bc27094725df1a6391d3c803c18049e1f46
Yoon Jinyeong <wwwz4805@naver.com>
2020-10-18 15:59:34 +0900

    Update DiaryView.swift

--- NoonBody/Views/Tabs/NoonBody/DiaryView.swift
+++ NoonBody/Views/Tabs/NoonBody/DiaryView.swift
@@ -13,24 +13,62 @@ struct DiaryView: View {
     var body: some View {
             
         ScrollView {
-            HStack{
-                    VStack{
-                        Text("어제")
-                        
-                        Image("body1")
-                            .resizable()
-                            .frame(width: 180, height: 300, alignment: .trailing)
-                    }
-                    VStack{
-                        Text("오늘")
-                            
-                        Image("body2")
-                            .resizable()
-                            .frame(width: 180, height: 300, alignment: .trailing)
-                        
+            VStack{
+                Text(getDate())
+                    
+                Image("body2")
+                    .resizable()
+                    .frame(width: .infinity, height: 300, alignment: .trailing)
+                    .padding(.leading)
+                    .padding(.trailing)
+                
+                HStack{
+                    Text("공복")
+                        .frame(width: 50, height: 30)
+                        .background(Color.white)
+                    Text("공개")
+                        .frame(width: 50, height: 30)
+                        .background(Color.white)
+                    Spacer()
+                }.padding()
+                
+                
+                Divider()
+                
+                VStack(alignment: .leading){
+                    HStack{
+                        Text("몸무게")
+                        Spacer()
+                        Text("0.00kg")
+//                        TextField("0.00kg", text: $weight)
+//                            .frame(width: 70)
                     }
                     
+                    Divider()
                     
+                    HStack{
+                        Text("골격근량")
+                        Spacer()
+                        Text("0.00kg")
+//                        TextField("0.00kg", text: $muscle)
+//                            .frame(width: 70)
+                    }
+                    
+                    Divider()
+                    
+                    HStack{
+                        Text("체지방량")
+                        Spacer()
+                        Text("0.00kg")
+//                        TextField("0.00kg", text: $fat)
+//                            .frame(width: 70)
+                    }
+                    
+                }.padding(.leading)
+                .padding(.trailing)
+                
+                Divider()
+                
             }
         }
             
@@ -38,9 +76,20 @@ struct DiaryView: View {
         
     }
 
+func getDate()->String{
+     let time = Date()
+     let timeFormatter = DateFormatter()
+     timeFormatter.dateFormat = ".date"
+    timeFormatter.dateStyle = .long
+    timeFormatter.locale = Locale(identifier: "Ko_KR")
+     let stringDate = timeFormatter.string(from: time)
+     return stringDate
+    }
 
 struct DiaryView_Previews: PreviewProvider {
     static var previews: some View {
         DiaryView()
     }
 }
+
+
