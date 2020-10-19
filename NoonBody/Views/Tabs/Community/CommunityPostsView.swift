92e01fc4d7a56c4d4794c0013c2c25e2800f485a
Yoon Jinyeong <wwwz4805@naver.com>
2020-10-19 15:06:42 +0900

    Update CommunityPostsView.swift

--- NoonBody/Views/Tabs/Community/CommunityPostsView.swift
+++ NoonBody/Views/Tabs/Community/CommunityPostsView.swift
@@ -10,11 +10,11 @@ import SwiftUI
 struct CommunityPostsView: View {
     
     var HomePosts: [recipePost] = [
-        recipePost(postingUser: "person", description: "This is a long description.This is a long description.This is a long description.This is a long description.", numberOfLikes: 1122, image: Image(systemName: "timelapse")),
-        recipePost(postingUser: "person2", description: "person2person2person2person2person2person2person2person2person2person2person2person2person2person2", numberOfLikes: 2122, image: Image(systemName: "timelapse")),
-        recipePost(postingUser: "person3", description: "person3person3person3person3person3person3person3person3person3person3person3person3person3", numberOfLikes: 3122, image: Image(systemName: "timelapse")),
-        recipePost(postingUser: "person4", description: "person4person4person4person4person4person4person4person4person4person4person4person4person4person4person4", numberOfLikes: 4122, image: Image(systemName: "timelapse")),
-        recipePost(postingUser: "person5", description: "person5person5person5person5person5person5person5person5person5person5person5person5person5person5person5", numberOfLikes: 5122, image: Image(systemName: "timelapse"))
+        recipePost(postingUser: "현아", description: "오늘 먹은 아침... 맛있어서 평소보다 많이 먹어버렸당ㅠ", numberOfLikes: 22, image: Image("food3").resizable()),
+        recipePost(postingUser: "혜리", description: "오늘의 눈바디! 어제보다 좀 빠진듯?", numberOfLikes: 54, image: Image("body3").resizable()),
+        recipePost(postingUser: "수빈", description: "오랜만에 진수성찬! 한식으로 맛나게 먹음~", numberOfLikes: 12, image: Image("food2").resizable()),
+        recipePost(postingUser: "현아", description: "이제 바디프로필 찍는 날까지 D-30! 한 달만 더 힘내자!", numberOfLikes: 30, image: Image("body1").resizable()),
+        recipePost(postingUser: "수지", description: "어제 많이 먹어서 오늘은 배 좀 나욤ㅋㅋㅋ", numberOfLikes: 66, image: Image("body2").resizable())
     ]
     
     var body: some View {
