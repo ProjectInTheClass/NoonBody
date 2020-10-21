3779d24a718b5d6d9a109ae937d7ef471e2ff21a
Yoon Jinyeong <wwwz4805@naver.com>
2020-10-21 13:57:19 +0900

    Update DiaryView.swift

--- NoonBody/Views/Tabs/NoonBody/DiaryView.swift
+++ NoonBody/Views/Tabs/NoonBody/DiaryView.swift
@@ -87,17 +87,7 @@ struct DiaryView: View {
 //    }
 
 
-func getDate(num:Int) -> String {
-    var dayComponent = DateComponents()
-    dayComponent.day = -num
-    let calendar = Calendar.current
-    let Day =  calendar.date(byAdding: dayComponent, to: Date())!
-    let formatter = DateFormatter()
-    formatter.locale = Locale(identifier: "Ko_KR")
-    formatter.dateFormat = "YYYY.MM.DD"
-    formatter.dateStyle = .medium
-    return formatter.string(from: Day) //Output is "March 6, 2020
-}
+
 
 struct DiaryView_Previews: PreviewProvider {
     static var previews: some View {
