6f6d83a8bf715d27586e0f812fcb63a201cb86f5
Yoon Jinyeong <wwwz4805@naver.com>
2020-10-22 11:34:47 +0900

    Update LoginView.swift

--- NoonBody/Views/LoginView.swift
+++ NoonBody/Views/LoginView.swift
@@ -6,54 +6,128 @@
 //
 
 import SwiftUI
+import Firebase
+import SPAlert
 
 struct LoginView: View {
+    @EnvironmentObject var env: GlobalEnvironment
+    
     @State private var signUp_visible = false
     
+    @State private var username: String = ""
+    @State private var password: String = ""
+    
+    @State private var isLoggedIn = false
+    
     var body: some View {
-        VStack(spacing:0){
-            Image("gray_background")
-                .resizable()
-                .scaledToFill()
-                .clipped()
-                .frame(height:350)
-                .frame(minWidth:0, maxWidth:.infinity)
-                .background(Color.blue)
-                .edgesIgnoringSafeArea(.top)
-            
-            TextField("Username", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
-                .padding()
-                .background(Color.clear)
-            TextField("Password", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
-                .padding()
-                .background(Color.clear)
-            
-            Spacer().frame(height:20)
-            
-            Button(action: {
+        NavigationView {
+            ZStack {
+                LinearGradient(gradient: Gradient(colors: [darkBlue, vDarkBlue]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
                 
-            }, label: {
-                HStack {
-                    Text("Login")
-                    Image(systemName: "arrow.right")
-                }.padding()
-            }).background(Color.init(red: 0.85, green: 0.85, blue: 0.85))
-            .cornerRadius(10)
-            
-            Spacer()
-            
-            Button(action: {
-                self.signUp_visible.toggle()
-            }, label: {
-                Text("Sign up?").padding()
-            }).background(Color.clear)
-            .foregroundColor(Color.init(red: 0.8, green: 0.8, blue: 0.8))
-            .sheet(isPresented: $signUp_visible, content: {
-                Text("this will be signup")
-            })
-            
-            Spacer()
-        }.background(Color.clear)
+                VStack(spacing:0){
+                    VStack {
+                        Text("체중보단 눈바디")
+                            .padding(15)
+                            .foregroundColor(Color("primaryOrange"))
+                        Text("NOONBODY")
+                            .font(.largeTitle)
+                            .foregroundColor(Color("primaryOrange"))
+                            .fontWeight(.bold)
+                    }.frame(height: 200)
+                    
+                    TextField("Username", text: $username)
+                        .autocapitalization(.none)
+                        .padding()
+                        .background(Color.clear)
+                        .frame(height: 50)
+                        .overlay(
+                            Capsule()
+                                .stroke(lineWidth: 2)
+                                .foregroundColor(.white))
+                        .padding()
+                        .foregroundColor(.white)
+                    
+                    SecureField("Password", text: $password)
+                        .autocapitalization(.none)
+                        .padding()
+                        .background(Color.clear)
+                        .frame(height: 50)
+                        .overlay(
+                            Capsule()
+                                .stroke(lineWidth: 2)
+                                .foregroundColor(.white))
+                        .padding()
+                    
+                    NavigationLink(
+                        destination: TabbedRootView(),
+                        isActive: $isLoggedIn){
+                        
+                        Button(action: {
+                            Firestore.firestore().collection("users").whereField("username", isEqualTo: self.username).getDocuments(){ (querySnapshot, err) in
+                                
+                                if let err = err{
+                                    print("Error getting documents: \(err)")
+                                }else{
+                                    if querySnapshot!.documents.count <= 0{
+                                        let alertView = SPAlertView(title: "No users found", message: "No users have the username you entered", preset: SPAlertPreset.error)
+                                        alertView.duration = 3
+                                        alertView.present()
+                                    }else if querySnapshot!.documents.count > 1{
+                                        let alertView = SPAlertView(title: "Something went wrong", message: "Multiple accounts exist with this username", preset: SPAlertPreset.error)
+                                        alertView.duration = 3
+                                        alertView.present()
+                                    }else {
+                                        
+                                        for document in querySnapshot!.documents {
+                                            print("\(document.documentID) => \(document.data())")
+                                            if document.data()["password"] as? String ?? "" == (self.password){
+                                                self.env.currentUser = user(
+                                                    username: document.data()["username"] as? String ?? "",
+                                                    password: document.data()["password"] as? String ?? "",
+                                                    name: document.data()["name"] as? String ?? "",
+                                                    email: document.data()["email"] as? String ?? ""
+                                                )
+                                                isLoggedIn = true
+                                            }else{
+                                                let alertView = SPAlertView(title: "password doesn't match", message: nil, preset: SPAlertPreset.error)
+                                                alertView.duration = 3
+                                                alertView.present()
+                                            }
+                                        }
+                                    }
+                                    
+                                }
+                            }
+                        }, label: {
+                            HStack {
+                                Text("Login")
+                                Image(systemName: "arrow.right")
+                            }.frame(height: 50)
+                            .frame(minWidth:0, maxWidth: .infinity)
+                            
+                        }).background(lightBlue).cornerRadius(25)
+                    }
+                    
+                    
+                    
+                    Spacer()
+                    
+                    Text("Don't have an account?").foregroundColor(.white)
+                    
+                    Button(action: {
+                        self.signUp_visible.toggle()
+                    }, label: {
+                        Text("Sign up?").padding()
+                    }).background(Color.clear)
+                    .foregroundColor(lightBlue)
+                    .sheet(isPresented: $signUp_visible, content: {
+                        SignUp()
+                    })
+                    
+                    Spacer()
+                }.background(Color.clear)
+            }
+        }
         
     }
 }
