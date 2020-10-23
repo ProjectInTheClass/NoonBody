5bdb3ea1d332fb676340716246e3198b615cca33
Yoon Jinyeong <wwwz4805@naver.com>
2020-10-23 12:11:26 +0900

    Change Settings folder

---
+++ NoonBody/Views/Tabs/Settings/SettingsAbout.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsAbout.swift
+//  NoonBody
+//
+//  Created by Jen on 2020-10-11.
+//
+
+import SwiftUI
+
+struct SettingsAbout: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsAbout_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsAbout()
+    }
+}
