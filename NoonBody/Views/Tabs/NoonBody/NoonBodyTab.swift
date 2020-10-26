e838c1d6ef21b7def31e5ee9f6d90b47675c3c4a
Yoon Jinyeong <wwwz4805@naver.com>
2020-10-26 20:45:17 +0900

    Update NoonBodyTab.swift

--- NoonBody/Views/Tabs/NoonBody/NoonBodyTab.swift
+++ NoonBody/Views/Tabs/NoonBody/NoonBodyTab.swift
@@ -31,22 +31,6 @@ struct NoonBodyTab: View {
         NavigationView {
             VStack {
                 
-//                NavigationLink(destination: NoonBodyWrite()) {
-//                    HStack {
-//                        Image(systemName: "square.and.pencil")
-//                            .font(.system(size: 30))
-//                        Text("눈바디 기록하기")
-//                            .bold()
-//                            .font(.system(size: 30))
-//                    }.frame(minWidth: 0, maxWidth: 250)
-//                    .padding()
-//                    .foregroundColor(.white)
-//                    .background(Color("primaryOrange"))
-//                    .cornerRadius(40)
-//                    .padding(.horizontal, 20)
-//                }
-                
-                
                 VStack(alignment: .center) {
                     SlidingTabView(selection: self.$selectedTabIndex, tabs: ["눈바디 일지", "모아보기"])
                     if selectedTabIndex == 0{
@@ -58,56 +42,6 @@ struct NoonBodyTab: View {
                     Spacer()
                 }
                     .animation(.none)
-                
-//                HStack {
-//                    Button(action: {
-//                        index = 0
-//                    }, label: {
-//
-//                        HStack{
-//                            Text("비교하기")
-//                                .bold()
-//                                .font(.system(size: 30))
-//                        }
-//
-//                    })
-//
-//                    .buttonStyle(ButtonSelected())
-//
-//
-//                    Button(action: {
-//
-//                        index = 1
-//
-//                    }, label: {
-//
-//                        HStack{
-//                            Text("모아보기")
-//                                .bold()
-//                                .font(.system(size: 30))
-//                        }
-//
-//                    }).buttonStyle(ButtonNonSelected())
-//                }
-                
-//                Divider().background(Color(.gray))
-                
-//                Picker(selection: $index, label: Text("")) {
-//                    ForEach(0..<contents.count) { index in
-//                        Text(self.contents[index]).tag(index)
-//                    }
-//                }.pickerStyle(SegmentedPickerStyle())
-//                .frame(height: 100)
-                
-//                Form{
-//                    if index == 0{
-//                        CompareView()
-//                    }else{
-//                        CollectionView()
-//                    }
-//
-//                }
-
                     
             }.background(Color("secondaryOrange"))
             .navigationTitle("눈바디")
@@ -144,38 +78,6 @@ fileprivate struct WriteButton: View{
     }
 }
 
-//fileprivate struct SelectedButton: View{
-//    @Binding var condition: Bool
-//
-//    var body: some View{
-//        Button(action: {
-//            self.condition = false
-//        }, label: {
-//            Text("취소")
-//                .bold()
-//                .font(.system(size: 30))
-//        })
-//        .frame(minWidth: 0, maxWidth: .infinity)
-//        .padding()
-//        .foregroundColor(Color("primaryOrange"))
-//    }
-//}
-//
-//fileprivate struct NonSelectedButton: View{
-//    @Binding var condition: Bool
-//
-//    var body: some View{
-//        Button(action: {
-//            self.condition = false
-//        }, label: {
-//            Text("저장")
-//                .bold()
-//                .font(.system(size: 30))
-//        })
-//        .frame(minWidth: 0, maxWidth: .infinity)
-//        .padding()
-//        .foregroundColor(.gray)
-//    }
-//}
+
 
 
