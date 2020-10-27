0b83b34c6db3e3548de718e8314b00b6fe468d71
Yoon Jinyeong <wwwz4805@naver.com>
2020-10-27 10:20:25 +0900

    Update NoonBodySetting login buttton

--- NoonBody/Views/Tabs/Settings/NoonBodySettings.swift
+++ NoonBody/Views/Tabs/Settings/NoonBodySettings.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct NoonBodySettings: View {
+    @State private var isLoggedIn = false
     
     @State var username: String = ""
     @State private var isPrivate: Bool = true
@@ -34,94 +35,112 @@ struct NoonBodySettings: View {
     
     var body: some View {
             NavigationView {
-
-                Form {
-                    Section {
-                        NavigationLink(destination: SettingsProfile()) {
-                            HStack {
-                                Image(systemName: "person.circle.fill")
-                                    .font(.system(size: 65))
-                                    .foregroundColor(.gray)
-                                VStack(alignment:.leading, spacing: 3) {
-                                    Text("UserName")
-                                        .font(.system(size:16))
-                                    Text("ID: abc123")
-                                        .font(.caption)
-                                        .foregroundColor(.secondary)
-                                    Text("오늘의 한마디: 팟팅!!")
-                                        .font(.caption)
-                                        .foregroundColor(.secondary)
+                VStack{
+                    Form {
+                        Section {
+                            NavigationLink(destination: SettingsProfile()) {
+                                HStack {
+                                    Image(systemName: "person.circle.fill")
+                                        .font(.system(size: 65))
+                                        .foregroundColor(.gray)
+                                    VStack(alignment:.leading, spacing: 3) {
+                                        Text("test1")
+                                            .font(.system(size:16))
+                                        Text("ID: abc123")
+                                            .font(.caption)
+                                            .foregroundColor(.secondary)
+                                        Text("오늘의 한마디: 팟팅!!")
+                                            .font(.caption)
+                                            .foregroundColor(.secondary)
+                                    }
                                 }
                             }
                         }
-                    }
-                
+                        
                     
-                    Section {
-                        DisclosureGroup(
-                            isExpanded: $isExpanded,
-                            content: {
-                                Toggle("눈바디 알림 수신", isOn: $isToggleNoonBody)
-                                Toggle("식단 알림 수신", isOn: $isToggleDiet)
-                                if isToggleDiet{
-                                    Toggle("아침 알림 수신", isOn: $isToggleBreakfast)
-                                    Toggle("점심 알림 수신", isOn: $isToggleLunch)
-                                    Toggle("저녁 알림 수신", isOn: $isToggleDinner)
+                        Section {
+                            DisclosureGroup(
+                                isExpanded: $isExpanded,
+                                content: {
+                                    Toggle("눈바디 알림 수신", isOn: $isToggleNoonBody)
+                                    Toggle("식단 알림 수신", isOn: $isToggleDiet)
+                                    if isToggleDiet{
+                                        Toggle("아침 알림 수신", isOn: $isToggleBreakfast)
+                                        Toggle("점심 알림 수신", isOn: $isToggleLunch)
+                                        Toggle("저녁 알림 수신", isOn: $isToggleDinner)
+                                    }
+                                    Toggle("친구 소식 알림 수신", isOn: $isToggleFriend)
+                                    
+    //                                ForEach(sites, id: \.self){site in
+    //                                    Text(site)
+    //                                }
+                                },
+                                label: {
+                                    HStack {
+                                        ZStack {
+                                            Image(systemName: "app.badge").font(.callout)
+                                        }.frame(width: 28, height: 28).background(Color.white).cornerRadius(6)
+                                        Text("알림 수신 설정")
+                                    }
                                 }
-                                Toggle("친구 소식 알림 수신", isOn: $isToggleFriend)
-                                
-//                                ForEach(sites, id: \.self){site in
-//                                    Text(site)
-//                                }
-                            },
-                            label: {
-                                HStack {
-                                    ZStack {
-                                        Image(systemName: "app.badge").font(.callout)
-                                    }.frame(width: 28, height: 28).background(Color.white).cornerRadius(6)
-                                    Text("알림 수신 설정")
-                                }
-                            }
-                        ).padding(.all, 10)
-                        
-                        
-                        
-//                        NavigationLink(destination: SettingsNotification()) {
-//                            HStack {
-//                                ZStack {
-//                                    Image(systemName: "app.badge").font(.callout)
-//                                }.frame(width: 28, height: 28).background(Color.white).cornerRadius(6)
-//                                Text("알림 설정")
-//                            }
-//                        }
-                        
-                        ContentView()
-                        
-//                        NavigationLink(destination: SettingsPhoto()) {
-//                            HStack {
-//                                ZStack {
-//                                    Image(systemName: "camera").font(.callout)
-//                                }.frame(width: 28, height: 28).background(Color.white).cornerRadius(6)
-//                                Text("사진 자동 저장")
-//                            }
-//                        }
-                        
-                       
-                        
-//                        NavigationLink(destination: SettingsAbout()) {
-//                            HStack {
-//                                ZStack {
-//                                    Image(systemName: "gear").font(.callout)
-//                                }.frame(width: 28, height: 28).background(Color.white).cornerRadius(6)
-//                                Text("눈바디 1.0")
-//                            }
-//                        }
+                            ).padding(.all, 10)
+                            
+                            
+                            
+    //                        NavigationLink(destination: SettingsNotification()) {
+    //                            HStack {
+    //                                ZStack {
+    //                                    Image(systemName: "app.badge").font(.callout)
+    //                                }.frame(width: 28, height: 28).background(Color.white).cornerRadius(6)
+    //                                Text("알림 설정")
+    //                            }
+    //                        }
+                            
+                            ContentView()
+                            
+    //                        NavigationLink(destination: SettingsPhoto()) {
+    //                            HStack {
+    //                                ZStack {
+    //                                    Image(systemName: "camera").font(.callout)
+    //                                }.frame(width: 28, height: 28).background(Color.white).cornerRadius(6)
+    //                                Text("사진 자동 저장")
+    //                            }
+    //                        }
+                            
+                           
+                            
+    //                        NavigationLink(destination: SettingsAbout()) {
+    //                            HStack {
+    //                                ZStack {
+    //                                    Image(systemName: "gear").font(.callout)
+    //                                }.frame(width: 28, height: 28).background(Color.white).cornerRadius(6)
+    //                                Text("눈바디 1.0")
+    //                            }
+    //                        }
+                        }
+                    }.background(Color("secondaryOrange"))
+                    .navigationTitle("마이페이지")
+                    
+
+                    .listStyle(GroupedListStyle())
+                    
+                    NavigationLink(destination: SettingsProfile(), isActive: $isLoggedIn) {
+                        Button(action: {
+                            self.isLoggedIn = true
+                        }, label: {
+                            HStack {
+                                Text("Login")
+                                Image(systemName: "arrow.right")
+                            }.frame(height: 50)
+                            .frame(minWidth:0, maxWidth: .infinity)
+                            .foregroundColor(Color.white)
+                            
+                        }).background(Color("primaryOrange")).cornerRadius(25).padding()
                     }
                 }.background(Color("secondaryOrange"))
-                .navigationTitle("마이페이지")
                 
-
-                .listStyle(GroupedListStyle())
+                
+                
             }.navigationBarTitle("").navigationBarHidden(true)
             .navigationViewStyle(StackNavigationViewStyle())
         }
