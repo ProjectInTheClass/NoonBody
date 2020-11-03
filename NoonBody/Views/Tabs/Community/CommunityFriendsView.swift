4b8e4341446109c0524659b6f335d37afc91aacb
Yoon Jinyeong <wwwz4805@naver.com>
2020-11-03 14:17:03 +0900

    Remove unused codes

--- NoonBody/Views/Tabs/Community/CommunityFriendsView.swift
+++ NoonBody/Views/Tabs/Community/CommunityFriendsView.swift
@@ -9,14 +9,6 @@ import SwiftUI
 
 struct CommunityFriendsView: View {
     
-//    @State var FriendLists: [FriendsPost] = [
-//        FriendsPost(friendName: "현아", friendDescription: "나는 현아", friendNumberOfLikes: 10, friendImage: ""),
-//        FriendsPost(friendName: "혜리", friendDescription: "나는 혜리", friendNumberOfLikes: 20, friendImage: ""),
-//        FriendsPost(friendName: "지현", friendDescription: "나는 지현", friendNumberOfLikes: 30, friendImage: ""),
-//        FriendsPost(friendName: "수지", friendDescription: "나는 수지", friendNumberOfLikes: 30, friendImage: ""),
-//        FriendsPost(friendName: "수빈", friendDescription: "나는 수빈", friendNumberOfLikes: 40, friendImage: "")
-//    ]
-    
     @ObservedObject var viewModel = CommunityFriendsViewModel()
     
     init() {
