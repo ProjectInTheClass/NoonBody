a8be4a4ca59a202193b4ec22301413d02d77df49
Yoon Jinyeong <wwwz4805@naver.com>
2020-11-03 16:13:21 +0900

    Update SettingsAbout.swift

--- NoonBody/Views/Tabs/Settings/SettingsAbout.swift
+++ NoonBody/Views/Tabs/Settings/SettingsAbout.swift
@@ -8,8 +8,60 @@
 import SwiftUI
 
 struct SettingsAbout: View {
+    
+    @State private var nickname=""
+    @State private var id=""
+    @State private var resolution=""
+    
+    private let oldPasswordToConfirmAgainst = "12345"
+    @State private var oldPassword = ""
+    @State private var newPassword = ""
+    @State private var confirmPassword = ""
+    
+    private func isUserInformationValid() -> Bool {
+        if nickname.isEmpty {
+            return false
+        }
+        
+        if id.isEmpty {
+            return false
+        }
+        
+        if resolution.isEmpty {
+            return false
+        }
+        
+        return true
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                Section(header: Text("나의 정보 수정하기")) {
+                    // Form fields go here
+                    TextField("닉네임", text: $nickname)
+                    TextField("ID", text: $id)
+                    TextField("나의 한마디!", text: $resolution)
+                    
+                    if self.isUserInformationValid() {
+                        Button(action: {
+                            print("Updated profile")
+                        }, label: {
+                            Text("나의 정보 업데이트 하기")
+                        })
+                    }
+                    
+                    
+                }
+                
+                Section(header: Text("비밀번호 변경하기")) {
+                    SecureField("현재 비밀번호를 입력하세요", text: $oldPassword)
+                    SecureField("새로운 비밀번호를 입력하세요", text: $newPassword)
+                    SecureField("새로운 비밀번호를 다시 한번 더 입력하세요", text: $confirmPassword)
+                    
+                }
+            }.navigationBarTitle("나의 프로필", displayMode: .inline)
+        }
     }
 }
 
