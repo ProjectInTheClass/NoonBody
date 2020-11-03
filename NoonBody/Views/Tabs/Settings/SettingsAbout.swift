06f4b945752c98825d6b949bb8eff5f1ed83f5b5
Yoon Jinyeong <wwwz4805@naver.com>
2020-11-03 16:59:04 +0900

    Update SettingsAbout.swift

--- NoonBody/Views/Tabs/Settings/SettingsAbout.swift
+++ NoonBody/Views/Tabs/Settings/SettingsAbout.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct SettingsAbout: View {
-    
+    // https://programmingwithswift.com/swiftui-form-beginners-guide/ 참고
     @State private var nickname=""
     @State private var id=""
     @State private var resolution=""
@@ -17,6 +17,8 @@ struct SettingsAbout: View {
     @State private var oldPassword = ""
     @State private var newPassword = ""
     @State private var confirmPassword = ""
+
+    @State private var keyboardOffset: CGFloat = 0
     
     private func isUserInformationValid() -> Bool {
         if nickname.isEmpty {
@@ -33,8 +35,22 @@ struct SettingsAbout: View {
         
         return true
     }
+
+    private func isPasswordValid() -> Bool {
+        if oldPassword != oldPasswordToConfirmAgainst {
+            return false
+        }
+        
+        if newPassword.isEmpty && newPassword == confirmPassword {
+            return true
+        }
+        
+        return false
+    }
     
+
     var body: some View {
+      VStack {
         NavigationView {
             Form {
                 Section(header: Text("나의 정보 수정하기")) {
@@ -57,12 +73,21 @@ struct SettingsAbout: View {
                 Section(header: Text("비밀번호 변경하기")) {
                     SecureField("현재 비밀번호를 입력하세요", text: $oldPassword)
                     SecureField("새로운 비밀번호를 입력하세요", text: $newPassword)
-                    SecureField("새로운 비밀번호를 다시 한번 더 입력하세요", text: $confirmPassword)
+                    SecureField("새로운 비밀번호를 한번 더 입력하세요", text: $confirmPassword)
+
+                    if self.isPasswordValid() {
+                      Button(action: {
+                        print("Updated password")
+                      }, label: {
+                        Text("비밀번호 변경하기")
+                      })
+                    }
                     
                 }
             }.navigationBarTitle("나의 프로필", displayMode: .inline)
         }
     }
+  }
 }
 
 struct SettingsAbout_Previews: PreviewProvider {
