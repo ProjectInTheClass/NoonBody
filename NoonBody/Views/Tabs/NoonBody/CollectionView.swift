2a9f0726fc9baf1709106f5a3d0d7eb7aa00b560
Yoon Jinyeong <wwwz4805@naver.com>
2020-11-12 18:35:09 +0900

    Update BarChart

--- NoonBody/Views/Tabs/NoonBody/CollectionView.swift
+++ NoonBody/Views/Tabs/NoonBody/CollectionView.swift
@@ -41,7 +41,7 @@ struct CollectionView: View {
                     HStack(alignment: .bottom){
                         ForEach(datas){ i in
                             
-                            Bar(weight: i.weight, day: i.day)
+                            BarChart(weight: i.weight, day: i.day)
                         }
                     }.frame(height: 150)
                     .padding(.leading, 20)
@@ -114,68 +114,7 @@ struct CollectionView_Previews: PreviewProvider {
     }
 }
 
-//Sample Datas
 
-struct type: Identifiable {
-    
-    var id: Int
-    var weight: CGFloat
-    var day: String
-}
-
-var datas = [
-    type(id: 0, weight: 50, day: "8월 3주"),
-    type(id: 1, weight: 48.2, day: "8월 4주"),
-    type(id: 2, weight: 47.9, day: "9월 1주"),
-    type(id: 3, weight: 46.4, day: "9월 2주"),
-    type(id: 4, weight: 46, day: "9월 3주"),
-    type(id: 5, weight: 45.5, day: "9월 4주"),
-    type(id: 6, weight: 45.3, day: "10월 1주")
-]
-
-
-struct Bar: View {
-    
-    var weight : CGFloat = 0
-    var day = ""
-    @State var myColor: String = "primaryGray"
-    @State var isChoosed: Bool = false
-    
-    var body: some View{
-        
-        VStack{
-            ZStack{
-                
-                Rectangle().fill(Color(myColor)).frame(width: UIScreen.main.bounds.width / 5 - 12, height: getHeight()).cornerRadius(10)
-                
-                Text(String(format: "%.1f", Double(weight)) + "kg").foregroundColor(Color.white)
-                    .padding(.bottom, 50)
-                    .font(.system(size: 10, weight: .bold))
-                
-            }.onTapGesture(count: 1, perform: {
-                self.isChoosed.toggle()
-                
-                if isChoosed{
-                    self.myColor = "primaryOrange"
-                }else{
-                    self.myColor = "primaryGray"
-                }
-                
-            })
-            
-            
-            Text(day).foregroundColor(Color.black.opacity(0.5))
-        }
-        
-        
-        
-    }
-    
-    func getHeight() -> CGFloat {
-        
-        return 200 / 100 * weight
-    }
-}
 
 
 //사진 불러오기
