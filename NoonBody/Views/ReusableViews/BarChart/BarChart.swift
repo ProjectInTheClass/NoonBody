0397cfbc496e201fe5e3594a2316f8e1f4c89c8a
Yoon Jinyeong <wwwz4805@naver.com>
2021-02-06 20:44:46 +0900

    Update BarChart.swift

--- NoonBody/Views/ReusableViews/BarChart/BarChart.swift
+++ NoonBody/Views/ReusableViews/BarChart/BarChart.swift
@@ -6,9 +6,44 @@
 //
 
 import SwiftUI
-
+import Charts
 
 struct BarChart: View {
+    
+    lazy var barChart : BarChartView = {
+        let barChartView = BarChartView()
+        barChartView.backgroundColor = .systemBlue
+        
+        barChartView.rightAxis.enabled = false
+        
+        //y축 custom
+        let yAxis = barChartView.leftAxis
+        yAxis.labelFont = .boldSystemFont(ofSize: 12)
+        yAxis.setLabelCount(6, force: false)
+        yAxis.labelTextColor = .white
+        yAxis.axisLineColor = .white
+        yAxis.labelPosition = .outsideChart
+        
+        //x축 custom
+        barChartView.xAxis.labelPosition = .bottom
+        barChartView.xAxis.labelFont = .boldSystemFont(ofSize: 12)
+        barChartView.xAxis.setLabelCount(6, force: false)
+        barChartView.xAxis.labelTextColor = .white
+        barChartView.xAxis.axisLineColor = .systemBlue
+        
+        barChartView.doubleTapToZoomEnabled = false
+        
+        // X축 레이블 포맷 지정
+//        barChartView.xAxis.valueFormatter = IndexAxisValueFormatter(values: workout_Data)
+        
+        // X축 레이블 갯수 최대로 설정 (이 코드 안쓸 시 Jan Mar May 이런식으로 띄엄띄엄 조금만 나옴)
+        barChartView.xAxis.setLabelCount(workout_Data.count, force: false)
+        
+        //chart animation
+        barChartView.animate(xAxisDuration: 2.0, yAxisDuration: 2.0)
+        
+        return barChartView
+    }()
 
     @State var selected = 0
     
