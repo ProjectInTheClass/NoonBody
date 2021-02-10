9dd682e3873961ebab0a38c4768f94629a20c29e
Yoon Jinyeong <wwwz4805@naver.com>
2021-02-11 08:53:59 +0900

    Update communityPost firebase filter

--- NoonBody/ViewModels/CommunityPostViewModel.swift
+++ NoonBody/ViewModels/CommunityPostViewModel.swift
@@ -11,26 +11,29 @@ import FirebaseFirestore
 class CommunityPostViewModel: ObservableObject {
   @Published var communityPosts = [CommunityPost]()
 
+    private var friendsList : [String] = ["현아", "혜리"]
     
   private var db = Firestore.firestore()
 
   func fetchData() {
-    db.collection("CommunityPost").addSnapshotListener { (querySnapshot, error) in
-      guard let documents = querySnapshot?.documents else {
-        print("No documents")
-        return
-      }
-
-      self.communityPosts = documents.map { queryDocumentSnapshot -> CommunityPost in
-        let data = queryDocumentSnapshot.data()
-        let postingUser = data["postingUser"] as? String ?? ""
-        let description = data["description"] as? String ?? ""
-        let numberOfLikes = data["numberOfLikes"] as? Int ?? 0
-        let image = data["image"] as? String ?? ""
-
+    //whereField 사용하여 filter 가능
+    //https://cloud.google.com/firestore/docs/query-data/queries
+    db.collection("CommunityPost").whereField("postingUser", in: [friendsList[1]]).addSnapshotListener { (querySnapshot, error) in
+        guard let documents = querySnapshot?.documents else {
+            print("No documents")
+            return
+        }
         
-        return CommunityPost(id: .init(), postingUser: postingUser, description: description, numberOfLikes: numberOfLikes, image: image)
-      }
+        self.communityPosts = documents.map { queryDocumentSnapshot -> CommunityPost in
+            let data = queryDocumentSnapshot.data()
+            let postingUser = data["postingUser"] as? String ?? ""
+            let description = data["description"] as? String ?? ""
+            let numberOfLikes = data["numberOfLikes"] as? Int ?? 0
+            let image = data["image"] as? String ?? ""
+            
+            
+            return CommunityPost(id: .init(), postingUser: postingUser, description: description, numberOfLikes: numberOfLikes, image: image)
+        }
     }
   }
 }
