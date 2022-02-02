9a71cf5b741268a6ccd54b0771f84ec827a20b1c
Yoon Jinyeong <wwwz4805@kakao.com>
2022-02-02 18:32:00 +0900

    Create DiaryView

---
+++ NoonBody/NoonBody/Views/Tabs/NoonBodyTab/DiaryView.swift
@@ -0,0 +1,69 @@
+//
+//  DiaryView.swift
+//  NoonBody
+//
+//  Created by 윤진영 on 2022/02/02.
+//
+
+import SwiftUI
+import Firebase
+
+struct DiaryView: View {
+    
+    
+    @ObservedObject var viewModel = NoonBodyViewModel()
+    
+    init() {
+        self.viewModel.fetchData()
+    }
+    
+    
+    var body: some View {
+        VStack{
+            
+            ScrollView{
+                if viewModel.isWrite == true{
+                    TabView{
+                    
+                        
+                            
+                            ForEach(viewModel.diaryPosts, id: \.id){ post in
+                                DiaryPostView(passed_diaryDate: post.diaryDate, passed_diaryFull: post.diaryFull, passed_diaryShare: post.diaryShare, passed_diaryWeight: post.diaryWeight, passed_diaryMuscle: post.diaryMuscle, passed_diaryFat: post.diaryFat, passed_diaryImage: post.diaryImage)
+                            }
+                        
+                        
+                } //: TAB
+                    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                
+                }else{
+                    Text("오늘의 눈바디를 기록하세요")
+                        .onTapGesture(count: 1, perform: {
+                            self.viewModel.fetchData()
+                        })
+                        
+                }
+            }
+
+
+            
+            
+            
+        }.onAppear(){
+            self.viewModel.fetchData()
+        }
+        
+            
+        
+            
+        }
+        
+    }
+
+
+
+struct DiaryView_Previews: PreviewProvider {
+    static var previews: some View {
+        DiaryView()
+    }
+}
+
